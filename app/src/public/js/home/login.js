5858058de9c5fdefeede3e4def119fbfe3bbffab
RDI <dkdlsekdls@naver.com>
2022-06-24 17:38:31 +0900

    로그인 성공 실패

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,4 +19,6 @@ function login() {
     },
     body: JSON.stringify(req),
   })
+    .then((res) => res.json())
+    .then(console.log)
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -11,9 +11,29 @@ const output = {
   },
 }
 
+const users = {
+  id: ['전유진', '김세정', '김범기'],
+  psword: ['1234', '1234', '1234'],
+}
+
 const process = {
   login: (req, res) => {
-    console.log(req.body)
+    const id = req.body.id,
+      psword = req.body.psword
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id)
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        })
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: '로그인에 실패하셨습니다',
+    })
   },
 }
 
